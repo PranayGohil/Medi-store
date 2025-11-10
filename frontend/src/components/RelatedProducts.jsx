1733719ba0dda6522366b3410a9e2ba49b98ab82
aniket_polara <polaraaniket007@gmail.com>
2025-11-10 11:27:46 +0530

    changes done by aniket

--- frontend/src/components/RelatedProducts.jsx
+++ frontend/src/components/RelatedProducts.jsx
@@ -1,15 +1,15 @@
-import React, { useContext } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Autoplay } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
+import React, { useContext, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
 import Title from "./Title";
 import ProductCard from "./ProductCard";
 
 const RelatedProducts = () => {
   const { products } = useContext(ShopContext);
+  const [visibleCount, setVisibleCount] = useState(12); // Show 12 initially
+
+  const handleLoadMore = () => {
+    setVisibleCount((prev) => prev + 12); // Load 12 more
+  };
 
   return (
     <section className="section-deal overflow-hidden py-[50px] max-[1199px]:py-[35px]">
@@ -21,29 +21,24 @@ const RelatedProducts = () => {
           description="Browse The Collection of Top Products."
         />
 
-        {/* Swiper Carousel */}
-        <Swiper
-          modules={[Navigation, Pagination, Autoplay]}
-          spaceBetween={20}
-          slidesPerView={2}
-          breakpoints={{
-            640: { slidesPerView: 2 },
-            768: { slidesPerView: 3 },
-            1024: { slidesPerView: 4 },
-          }}
-          autoplay={{ delay: 3000 }}
-          className=""
-        >
-          {products?.map((product) => (
-            <SwiperSlide
-              className={`grid
-                grid-cols-2 lg:grid-cols-3 gap-6
-              `}
-            >
-              <ProductCard {...product} />
-            </SwiperSlide>
+        {/* Grid Layout */}
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-6 p-3">
+          {products?.slice(0, visibleCount).map((product) => (
+            <ProductCard key={product._id} {...product} />
           ))}
-        </Swiper>
+        </div>
+
+        {/* Load More Button */}
+        {visibleCount < products.length && (
+          <div className="flex justify-center text-blue-600 w-full mt-[30px]">
+            <button
+              onClick={handleLoadMore}
+              className="block text-center mt-2 bb-btn-2 transition-all duration-[0.3s] ease-in-out font-Poppins leading-[28px] tracking-[0.03rem] py-[4px] px-[25px] text-[14px] font-normal text-[#0097b2] bg-[#ffffff] rounded-[10px] border-[1px] border-solid border-[#0097b2] hover:bg-[#0097b2] hover:border-[#ffffff] hover:text-[#ffffff]"
+            >
+              Load More
+            </button>
+          </div>
+        )}
       </div>
     </section>
   );
